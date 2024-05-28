ac35f898c0d48eb576b9761caf128778ae61c4e6
Mario Graff <mgraffg@ieee.org>
2024-05-28 11:10:20 -0600

    URL

--- dialectid/tests/test_utils.py
+++ dialectid/tests/test_utils.py
@@ -19,7 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
+# https://www.cia.gov/the-world-factbook/about/archives/2021/field/languages/
 
 from dialectid import utils
 
