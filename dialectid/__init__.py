bf577fa3d855a4986bc6d7a71d0acfca4d202dbb
Mario Graff <mgraffg@ieee.org>
2025-07-22 06:51:15 -0500

    uniform_distribution option

--- dialectid/__init__.py
+++ dialectid/__init__.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
-# from dialectid.text_repr import BoW, SeqTM
+# from dialectid.text_repr import BoW, SeqTM
 from dialectid.model import DialectId, BoW
