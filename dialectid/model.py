e9db9eadcfbe927dc8bb8eb8d77e675b96afdaa8
Mario Graff <mgraffg@ieee.org>
2025-09-22 06:46:32 -0500

    Fixed training size

--- dialectid/model.py
+++ dialectid/model.py
@@ -56,6 +56,7 @@ class DialectId(EncExpT):
     with_intercept: bool=True
     probability: bool=False
     uniform_distribution: bool=True
+    fixed_training_size: bool=False
 
     def identifier_filter(self, key, value):
         """Test default parameters"""
